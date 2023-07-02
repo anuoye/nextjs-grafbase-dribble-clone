522fc1412b29a0ff558345d88897e918a8a05a81
Anu Oyebanjo <anuoyebanjodunmi660@gmail.com>
2023-07-02 07:02:09 +0100

    first commit

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,34 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(()=> Project).list().optional(),
+})
+
+
+// @ts-ignore
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+
+})
+
+
+
+
+
+
+export default config({
+  schema: g
+})
